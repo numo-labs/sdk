cbf10282d22902a7a1027716c8764b07e78d4d8d
Leonard Martin <leonard.martin@gmail.com>
2016-09-02 15:43:59 +0100

    Add tests for concurrent queries

--- test/query.test.js
+++ test/query.test.js
@@ -24,92 +24,124 @@ describe('query', () => {
     server.stop();
   });
 
-  it('emits a `result` event for each result', (done) => {
-    const stub = sinon.stub();
-    const q = sdk.query()
-      .on('result', stub)
-      .on('end', () => {
-        expect(stub.callCount).to.equal(3);
-        done();
-      });
+  describe('results', () => {
 
-    server.emit(q.id, 'result', { data: { result: 1 } });
-    server.emit(q.id, 'result', { data: { result: 2 } });
-    server.emit(q.id, 'result', { data: { result: 3 } });
-    server.emit(q.id, 'end');
+    it('emits a `result` event for each result', (done) => {
+      const stub = sinon.stub();
+      const q = sdk.query()
+        .on('result', stub)
+        .on('end', () => {
+          expect(stub.callCount).to.equal(3);
+          done();
+        });
+
+      server.emit(q.id, 'result', { data: { result: 1 } });
+      server.emit(q.id, 'result', { data: { result: 2 } });
+      server.emit(q.id, 'result', { data: { result: 3 } });
+      server.emit(q.id, 'end');
+    });
+
+    it('passes `data` from each result event to `result` event as argument', (done) => {
+      const stub = sinon.stub();
+      const q = sdk.query()
+        .on('result', stub)
+        .on('end', () => {
+          expect(stub.getCall(0).args[0]).to.deep.equal({ result: 1 });
+          expect(stub.getCall(1).args[0]).to.deep.equal({ result: 2 });
+          expect(stub.getCall(2).args[0]).to.deep.equal({ result: 3 });
+          done();
+        });
+
+      server.emit(q.id, 'result', { data: { result: 1 } });
+      server.emit(q.id, 'result', { data: { result: 2 } });
+      server.emit(q.id, 'result', { data: { result: 3 } });
+      server.emit(q.id, 'end');
+    });
+
+    it('parallel queries emit events only for their own results', (done) => {
+      const stub1 = sinon.stub();
+      const q1 = sdk.query()
+        .on('result', stub1)
+      const stub2 = sinon.stub();
+      const q2 = sdk.query()
+        .on('result', stub2)
+        .on('end', () => {
+          expect(stub1.callCount).to.equal(1);
+          expect(stub2.callCount).to.equal(1);
+          expect(stub1.getCall(0).args[0]).to.deep.equal({ result: 1 });
+          expect(stub2.getCall(0).args[0]).to.deep.equal({ result: 2 });
+          done();
+        });
+
+      server.emit(q1.id, 'result', { data: { result: 1 } });
+      server.emit(q2.id, 'result', { data: { result: 2 } });
+      server.emit(q2.id, 'end');
+    });
+
   });
 
-  it('passes `data` from each result event to `result` event as argument', (done) => {
-    const stub = sinon.stub();
-    const q = sdk.query()
-      .on('result', stub)
-      .on('end', () => {
-        expect(stub.getCall(0).args[0]).to.deep.equal({ result: 1 });
-        expect(stub.getCall(1).args[0]).to.deep.equal({ result: 2 });
-        expect(stub.getCall(2).args[0]).to.deep.equal({ result: 3 });
-        done();
-      });
+  describe('timeouts', () => {
 
-    server.emit(q.id, 'result', { data: { result: 1 } });
-    server.emit(q.id, 'result', { data: { result: 2 } });
-    server.emit(q.id, 'result', { data: { result: 3 } });
-    server.emit(q.id, 'end');
+    it('emits a `timeout` event if no `end` event is received within 30 seconds', (done) => {
+      const clock = sinon.useFakeTimers();
+      const q = sdk.query()
+        .on('timeout', () => {
+          clock.restore();
+          done();
+        })
+        .on('end', () => {
+          done(new Error('should not be called'));
+        });
+
+      clock.tick(31000);
+      server.emit(q.id, 'end');
+    });
+
+    it('emits a `timeout` event if no `end` event is received within the configured tiemout for that request', (done) => {
+      const clock = sinon.useFakeTimers();
+      const q = sdk.query({}, { timeout: 10 })
+        .on('timeout', () => {
+          clock.restore();
+          done();
+        })
+        .on('end', () => {
+          done(new Error('should not be called'));
+        });
+
+      clock.tick(11000);
+      server.emit(q.id, 'end');
+    });
+
+    it('does not emit a `timeout` event if `end` event is received within the configured timeout for that request', (done) => {
+      const q = sdk.query({})
+        .on('timeout', () => {
+          done(new Error('should not be called'));
+        })
+        .on('end', () => {
+          done();
+        });
+
+      server.emit(q.id, 'end');
+    });
+
   });
 
-  it('emits a `timeout` event if no `end` event is received within 30 seconds', (done) => {
-    const clock = sinon.useFakeTimers();
-    const q = sdk.query()
-      .on('timeout', () => {
-        clock.restore();
-        done();
-      })
-      .on('end', () => {
-        done(new Error('should not be called'));
-      });
+  describe('errors', () => {
 
-    clock.tick(31000);
-    server.emit(q.id, 'end');
-  });
+    it('emits an error event if no providers are able to handle the query', (done) => {
+      // setting `handle` param to false will make the mock provider not handle the query
+      // see ./test/helpers/backend.js
+      const q = sdk.query({ handle: false })
+        .on('error', (e) => {
+          done();
+        })
+        .on('end', () => {
+          done(new Error('should not be called'));
+        });
 
-  it('emits a `timeout` event if no `end` event is received within the configured tiemout for that request', (done) => {
-    const clock = sinon.useFakeTimers();
-    const q = sdk.query({}, { timeout: 10 })
-      .on('timeout', () => {
-        clock.restore();
-        done();
-      })
-      .on('end', () => {
-        done(new Error('should not be called'));
-      });
+      server.emit(q.id, 'end');
+    });
 
-    clock.tick(11000);
-    server.emit(q.id, 'end');
-  });
-
-  it('does not emit a `timeout` event if `end` event is received within the configured timeout for that request', (done) => {
-    const q = sdk.query({})
-      .on('timeout', () => {
-        done(new Error('should not be called'));
-      })
-      .on('end', () => {
-        done();
-      });
-
-    server.emit(q.id, 'end');
-  });
-
-  it('emits an error event if no providers are able to handle the query', (done) => {
-    // setting `handle` param to false will make the mock provider not handle the query
-    // see ./test/helpers/backend.js
-    const q = sdk.query({ handle: false })
-      .on('error', (e) => {
-        done();
-      })
-      .on('end', () => {
-        done(new Error('should not be called'));
-      });
-
-    server.emit(q.id, 'end');
   });
 
 });
