d7aef863b0f16eccac986100e91e9432b59d2fbf
Marcelo Gomez <gomezconesa.marcelo@gmail.com>
2016-10-03 12:08:24 +0200

    Autocomplete basic test on sdk index test

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,9 +4,17 @@ var test = require('tape');
 var SDK = require('../index');
 
 test('SDK', function (t) {
-    t.plan(2);
-    var sdk = SDK.init('uk');
-    t.notEqual(sdk.config, null, 'Once initialized with a valid market, config is NOT null');
-    var sdk = SDK.init('invalid market');
-    t.equal(sdk.config, null, 'Once initialized with a non valid market, config IS null');
+    t.plan(4);
+    var sdk1 = SDK.init('uk');
+    t.notEqual(sdk1.config, null, 'Once initialized with a valid market, config is NOT null');
+    
+    var autocomplete = sdk1.autocomplete('departure_airport');
+    autocomplete.for('City', function (err, elements) {
+        t.equal(err, null, 'Autocomplete should not produce error');
+        t.deepEqual(elements, ['London City'], 'Autocomplete should filter departure airports based on the input ');
+    });
+    
+    var sdk2 = SDK.init('invalid market');
+    t.equal(sdk2.config, null, 'Once initialized with a non valid market, config IS null');
+    
 });
