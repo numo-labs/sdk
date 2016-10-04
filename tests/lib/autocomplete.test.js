6a3378ba3d93d43227a18af6563a91a9c060d04b
Marcelo Gomez <gomezconesa.marcelo@gmail.com>
2016-10-04 09:44:46 +0200

    Autocomplete case insensitive

--- tests/lib/autocomplete.test.js
+++ tests/lib/autocomplete.test.js
@@ -19,11 +19,15 @@ test('Autocomplete', function (t) {
         });
     });
     t.test('Destination', function (st) {
-        st.plan(3);
+        st.plan(5);
         var autoComplete = autocomplete('destination', ukConfig);
         autoComplete.for('Islan', 'London City', function (err, elements) {
             st.equal(err, null, 'Should not return an error if departure airport is valid');
             st.deepEqual(elements, ['Balearic Islands'], 'Should filter destination airports based on the input and the departure airport ');
+        });
+        autoComplete.for('', 'London City', function (err, elements) {
+            st.equal(err, null, 'Should not return an error if departure airport is valid');
+            st.deepEqual(elements, ['Balearic Islands', 'New York', 'Portugal'], 'Should show all the available destinations given a departure airport if empty input provided');
         });
         autoComplete.for('London', 'Unexistent departure airport', function (err) {
             st.notEqual(err, null, 'Should return an error if departure airport is not valid');
